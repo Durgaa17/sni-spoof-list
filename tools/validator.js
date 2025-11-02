c6ddb385569505055465ae7b41300484055259e8
Durgaa17 <sivaramhome86@gmail.com>
2025-11-02 23:43:31 +0800

    Update validator.js

--- tools/validator.js
+++ tools/validator.js
@@ -1,13 +1,9 @@
-// tools/validator.js - Main validator coordinator
+// tools/validator.js - Main coordinator
 import { getValidatorHTML } from './validator-core.js';
 import { initValidatorUI } from './validator-ui.js';
 
 export default function initValidator(container) {
     console.log('Config Validator tool loaded successfully!');
-    
-    // Add HTML to container
     container.insertAdjacentHTML('beforeend', getValidatorHTML());
-    
-    // Initialize UI event handlers
     initValidatorUI(container);
 }
